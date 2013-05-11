c11c17304af3981017d41685dbb3469fc95b6bef
Jason Draper <jason@drapergeek.com>
2013-05-11 13:28:35 -0400

    Return an error if the current url cant be set.

--- app/controllers/admin/api/current_urls_controller.rb
+++ app/controllers/admin/api/current_urls_controller.rb
@@ -1,7 +1,10 @@
 class Admin::Api::CurrentUrlsController < Admin::ApiController
   def create
     unit = Unit.find_by_name(params[:unit_id])
-    unit.set_current_url(params[:url])
-    render json: :success
+    if unit && unit.set_current_url(params[:url])
+      render json: :success
+    else
+      render json: { errors: I18n.t('units.set_current_url.error') }, status: :forbidden
+    end
   end
 end
