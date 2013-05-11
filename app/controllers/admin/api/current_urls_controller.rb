40650fe06e7edf88149103dd1f072b707ec51005
Jason Draper <jason@drapergeek.com>
2013-05-11 15:14:41 -0400

    Update current_urls to use respond_with

--- app/controllers/admin/api/current_urls_controller.rb
+++ app/controllers/admin/api/current_urls_controller.rb
@@ -1,10 +1,12 @@
 class Admin::Api::CurrentUrlsController < Admin::ApiController
+  respond_to :json
+
   def create
     unit = Unit.find_by_name(params[:unit_id])
     if unit && unit.set_current_url(params[:url])
-      render json: :success
+      respond_with unit, location: nil
     else
-      render json: { errors: I18n.t('units.set_current_url.error') }, status: :forbidden
+      render json: { errors: I18n.t('units.set_current_url.error') }, status: 422
     end
   end
 end
